0c4d9916df803bdf2c14bc940b15280db20573b3
Ashvin Panicker <agnjkafgh@gmail.com>
2025-02-05 03:52:53 +0530

    add animation

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -21,7 +21,14 @@ const Accordion: React.FC<AccordionProps> = ({ index, title, desc, isOpen, setIs
                 <b>{title}</b>
                 <button className="text-blue-500 text-3xl" onClick={handleAccordionState}>{isOpen[index] ? "-" : "+"}</button>
             </div>
-            {isOpen[index] && <div>{desc}</div>}
+            <div 
+                className={`overflow-hidden transition-all duration-300 ease-in-out 
+                ${isOpen[index] ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'}`}
+            >
+                <div className="py-2">
+                    {desc}
+                </div>
+            </div>
         </div>
     );
 };
